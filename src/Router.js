a80c194b2d78fde5494d0d4cbd892e801319de75
tcannetti <tcannetti@gmail.com>
2021-04-20 20:03:23 -0500

    router done

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Login from './containers/Login'
 import Add from './containers/Add'
+import Main from './containers/Main'
+import BusinessDetail from './containers/BusinessDetail'
 
 import { Switch, Route } from 'react-router'
 
 export default function Router() {
   return (
     <Switch>
-      <Route />
-      <Route />
-      <Route />
-      <Route />
+      <Route exact path='/' component={Main} />
+      <Route exact path='/add' component={Add} />
+      <Route exact path='login' component={Login} />
+      <Route exact path='business:id' component={BusinessDetail} />
     </Switch>
   )
 }
